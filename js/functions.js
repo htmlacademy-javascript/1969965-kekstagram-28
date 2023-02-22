5d5eb5ecad546a80afc0e83356093a2d44d4bea5
JulianaAstra <julianaas@yandex.ru>
2023-02-22 16:50:28 +0300

    добавит исправления по замечаниям

--- js/functions.js
+++ js/functions.js
@@ -20,45 +20,50 @@ isPalindrome('Alla');
 // Третья функция
 
 const getNumberFromString = (phrase) => {
-
   if (!isNaN(phrase)) {
     if (!Number.isInteger(phrase)) {
       return Number(String(phrase).replaceAll('.', ''));
     }
     return Math.abs(Number(phrase));
-  } else if (Array.from(phrase.replaceAll('.', '')).every((element) => isNaN(element))) {
+  }
+  if (Array.from(phrase.replaceAll(' ', '')).every((element) => isNaN(element))) {
     return NaN;
   }
 
-  return Number(Array.from(phrase.replaceAll('.', '')).filter((element) => Number(element) >= 0).join(''));
+  return Number(Array.from(phrase.replaceAll(' ', '')).filter((element) => Number(element) >= 0).join(''));
 };
 
+getNumberFromString('1 кефир, 0.5 батона');
 getNumberFromString(1.5);
 getNumberFromString('2023');
 getNumberFromString(-1);
+
 
 // Четвёртая функция
 
 const padString = (string, minLength, padChar) => {
 
-  if (string.length < minLength) {
-    const lengthNeeded = minLength - string.length;
-    if (padChar.length > lengthNeeded) {
-      return padChar.slice(0, lengthNeeded) + string;
-    } else if (padChar.length === lengthNeeded) {
-      return padChar + string;
-    } else if (padChar.length < lengthNeeded) {
-      const charsDivision = lengthNeeded / padChar.length;
-      let result = padChar.repeat(charsDivision) + string;
-      if (lengthNeeded % padChar.length !== 0) {
-        result = padChar.repeat(Math.floor(charsDivision)) + string;
-        const charsToAdd = lengthNeeded - result.length;
-        return padChar.slice(0, charsToAdd - 1) + result;
-      }
-      return result;
-    }
+  if (string.length >= minLength) {
+    return string;
   }
-  return string;
+
+  const lengthNeeded = minLength - string.length;
+  if (padChar.length > lengthNeeded) {
+    return padChar.slice(0, lengthNeeded) + string;
+  }
+  if (padChar.length === lengthNeeded) {
+    return padChar + string;
+  }
+  if (padChar.length < lengthNeeded) {
+    const charsDivision = lengthNeeded / padChar.length;
+    let result = padChar.repeat(charsDivision) + string;
+    if (lengthNeeded % padChar.length !== 0) {
+      result = padChar.repeat(Math.floor(charsDivision)) + string;
+      const charsToAdd = lengthNeeded - result.length;
+      return padChar.slice(0, charsToAdd - 1) + result;
+    }
+    return result;
+  }
 };
 
 padString('q', 4, 'we');
