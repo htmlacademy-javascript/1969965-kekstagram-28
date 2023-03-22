3ff52f048c4bc4e80beb496c8f0cad01521ecd28
Julia Karnausova <julianaas@yandex.ru>
2023-03-22 19:00:55 +0300

    добавит элементы для работы с галереей

--- js/gallery.js
+++ js/gallery.js
@@ -7,6 +7,7 @@ const bigPicture = document.querySelector('.big-picture__img img');
 const likesCount = bigPictureModal.querySelector('.likes-count');
 const commentsCount = bigPictureModal.querySelector('.comments-count');
 const socialCaption = bigPictureModal.querySelector('.social__caption');
+const socialCommentsContainer = bigPictureModal.querySelector('.social__comments');
 
-export { thumbnailsList, bigPictureModal, pictureCancelButton, socialCommentCount, commentsLoader, bigPicture, likesCount, commentsCount, socialCaption };
+export { thumbnailsList, bigPictureModal, pictureCancelButton, socialCommentCount, commentsLoader, bigPicture, likesCount, commentsCount, socialCaption, socialCommentsContainer };
 
