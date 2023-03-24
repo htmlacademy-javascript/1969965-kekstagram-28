774dffb2ac284bdcd2b847267c636fc1896f3b27
JulianaAstra <julianaas@yandex.ru>
2023-03-24 15:10:07 +0300

    добавит импорт функции для корректной работы модуля

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 import { renderGallery } from './render-gallery.js';
 import { makeThumbnails } from './make-thumbnails.js';
+import { changeScaleValue } from './change-scale.js';
 
 renderGallery(makeThumbnails());
