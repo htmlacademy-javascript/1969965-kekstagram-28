c66f355bcd46c2d9ddc21de1762063bd09523cec
JulianaAstra <julianaas@yandex.ru>
2023-03-25 22:07:06 +0300

    уберет лишнее

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 import { renderGallery } from './render-gallery.js';
 import { makeThumbnails } from './make-thumbnails.js';
-import { changeScaleValue } from './change-scale.js';
 
 renderGallery(makeThumbnails());
