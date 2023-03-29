ab5c10e1a7cc9856dcff4b4e795989161f40ffa8
Keks <keks@htmlacademy.ru>
2023-03-29 07:55:14 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/main.js
@@ -0,0 +1,6 @@
+import { renderGallery } from './render-gallery.js';
+import { makeThumbnails } from './make-thumbnails.js';
+
+// комментарий для задания 9.16 Помощь друга
+
+renderGallery(makeThumbnails());
