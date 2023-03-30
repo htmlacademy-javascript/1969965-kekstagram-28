76a1575835cd850b26d045599170d0e080ba773d
JulianaAstra <julianaas@yandex.ru>
2023-03-31 02:44:23 +0300

    добавит константы

---
+++ js/api/constants-for-api.js
@@ -0,0 +1,17 @@
+const BASE_URL = 'https://28.javascript.pages.academy/kekstagram';
+const Route = {
+  GET_DATA: '/data',
+  SEND_DATA: '/'
+};
+
+const Method = {
+  GET: 'GET',
+  POST: 'POST'
+};
+
+const SubmitButtonText = {
+  IDLE: 'Опубликовать',
+  SENDING: 'Публикую...'
+};
+
+export {BASE_URL, Route, Method, SubmitButtonText};
